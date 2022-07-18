c05d0980ef8ed7eaca0a8fbfe22152eb4efdab30
kurtzkaranq <mendbayarerxes@gmail.com>
2022-07-18 10:58:37 +0800

    add

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
-const http = require("http");
 const fs = require("fs");
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+app.use(express.static("public"));
 
 app.get("/api", (request, response) => {
+  console.log(process.env.PORT);
   fs.readFile("./models/models.json", "utf-8", (err, data) => {
     if (err) {
       console.log("error on reading");
--- frontend/front/src/components/Header.jsx
+++ frontend/front/src/components/Header.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import '../style/header.css'
+import React from "react";
+// import '../style/header.css'
 export default function Header() {
-    return (
-        <div className='flex W-100 justify-around text-gray-400 header-character header pb-3'>
-            <h5>
-                ERKHES MENDBAYAR
-            </h5>
-            <div className=' justify-evenly  w-72 '>
-                <a href="" className=' w-20 mr-10'>PROFILE</a>
-                <a href="" className='mr-10'>WORK</a>
-                <a href="" className='mr-10'>CONTACT</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex W-100 justify-around text-gray-400 header-character header pb-3">
+      <h5>ERKHES MENDBAYAR</h5>
+      <div className=" justify-evenly  w-72 ">
+        <a href="" className=" w-20 mr-10">
+          PROFILE
+        </a>
+        <a href="" className="mr-10">
+          WORK
+        </a>
+        <a href="" className="mr-10">
+          CONTACT
+        </a>
+      </div>
+    </div>
+  );
 }
